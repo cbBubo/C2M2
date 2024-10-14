7d51d5a505d105674c544eebd8c6c872c1419f67
cbBubo <cari@bubold.com>
2024-10-14 10:14:11 -0500

    Locked Player 10/14/24

---
+++ story_content/5jnrhwQbQ3f_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.80.31058.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.213%0D%0ALesson%203:%20Management%20Roles%20and%20Responsibilities%0D%0A%0D%0A"}')
